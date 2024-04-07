ce22a579dd6e12b48132740e52a22de4adcdbc91
AidaTheShip <a.baradari2412@gmail.com>
2024-04-07 10:11:23 -0400

    Updating visualization function for reinforcement learning

--- Reinforcement Learning/visualization.py
+++ Reinforcement Learning/visualization.py
@@ -1,24 +1,13 @@
-# We have to import our Mujoco code into a gynasium environment to use stable baselines
-# make sure that you have pip install gymnasium[mujoco] 
-#import stable_baselines3
-#import gymnasium
-import mujoco as mj
-import random 
+import time 
+import itertools 
+import numpy as np
+import mediapy as media
+import matplotlib.pyplot as plt
+import mujoco
+# import distutils.util
 import os
-from mujoco.glfw import glfw
-
-# Setting initial states
-button_left = False
-button_middle = False
-button_right = False
-lastx = 0
-lasty = 0
-
-# Making the necessary directories for our models and logs
-model_dir = "models"
-log_dir = "logs"
-os.makedirs(model_dir, exist_ok=True)
-os.makedirs(log_dir, exist_ok=True)
+import subprocess
+np.set_printoptions(precision=3, suppress=True, linewidth=100)
 
 # SETTING BASIC PROPERTIES
 xml_path = "cyr_wheel.xml" # Setting the path for the model
@@ -27,131 +16,43 @@ abspath = os.path.join(dirname + "/" + xml_path) # making sure that it aligns wi
 xml_path = abspath
 simulation_time = 1000
 
-def keyboard(window, key, scancode, act, mods):
-    if act == glfw.PRESS and key == glfw.KEY_BACKSPACE:
-        mj.mj_resetData(model, data)
-        mj.mj_forward(model, data)
+model = mujoco.MjModel.from_xml_path(xml_path) # this makes a model out of the xml such that we can use it. 
+data = mujoco.MjData(model)
 
-def mouse_button(window, button, act, mods):
-    # update button state
-    global button_left
-    global button_middle
-    global button_right
+# Make renderer, render and show the pixels
+renderer = mujoco.Renderer(model)
+# media.show_image(renderer.render())
 
-    button_left = (glfw.get_mouse_button(
-        window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS)
-    button_middle = (glfw.get_mouse_button(
-        window, glfw.MOUSE_BUTTON_MIDDLE) == glfw.PRESS)
-    button_right = (glfw.get_mouse_button(
-        window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS)
+mujoco.mj_forward(model, data)
+renderer.update_scene(data, "track")
 
-def mouse_move(window, xpos, ypos):
-    # compute mouse displacement, save
-    global lastx
-    global lasty
-    global button_left
-    global button_middle
-    global button_right
+# media.show_image(renderer.render())
 
-    dx = xpos - lastx
-    dy = ypos - lasty
-    lastx = xpos
-    lasty = ypos
+def render(xml, duration, framerate, camera):
+    model = mujoco.MjModel.from_xml_path(xml) # this makes a model out of the xml such that we can use it. 
 
-    # no buttons down: nothing to do
-    if (not button_left) and (not button_middle) and (not button_right):
-        return
+    data = mujoco.MjData(model)
 
-    # get current window size
-    _, height = glfw.get_window_size(window)
+    # Make renderer, render and show the pixels
+    renderer = mujoco.Renderer(model)
+    # media.show_image(renderer.render())
 
-    # get shift key state
-    PRESS_LEFT_SHIFT = glfw.get_key(
-        window, glfw.KEY_LEFT_SHIFT) == glfw.PRESS
-    PRESS_RIGHT_SHIFT = glfw.get_key(
-        window, glfw.KEY_RIGHT_SHIFT) == glfw.PRESS
-    mod_shift = (PRESS_LEFT_SHIFT or PRESS_RIGHT_SHIFT)
+    mujoco.mj_forward(model, data)
+    renderer.update_scene(data)
 
-    # determine action based on mouse button
-    if button_right:
-        if mod_shift:
-            action = mj.mjtMouse.mjMOUSE_MOVE_H
-        else:
-            action = mj.mjtMouse.mjMOUSE_MOVE_V
-    elif button_left:
-        if mod_shift:
-            action = mj.mjtMouse.mjMOUSE_ROTATE_H
-        else:
-            action = mj.mjtMouse.mjMOUSE_ROTATE_V
-    else:
-        action = mj.mjtMouse.mjMOUSE_ZOOM
+    #media.show_image(renderer.render())
 
-    mj.mjv_moveCamera(model, action, dx/height,
-                      dy/height, scene, cam)
+    # Simulate and display video.
+    frames = []
+    mujoco.mj_resetDataKeyframe(model, data, 0)  # Reset the state to keyframe 0
+    while data.time < duration:
+        mujoco.mj_step(model, data)
+        if len(frames) < data.time * framerate:
+            renderer.update_scene(data, camera)
+            pixels = renderer.render()
+            frames.append(pixels)
 
-def scroll(window, xoffset, yoffset):
-    action = mj.mjtMouse.mjMOUSE_ZOOM
-    mj.mjv_moveCamera(model, action, 0.0, -0.05 *
-                      yoffset, scene, cam)
+    # media.show_video(frames, fps=framerate)
+    media.write_video('v.mp4', frames, fps=60, qp=18)
 
-
-
-# Mujoco data structures
-model = mj.MjModel.from_xml_path(xml_path)
-data = mj.MjData(model)
-cam = mj.MjvCamera()
-opt = mj.MjvOption() #Visualization options
-print_camera_config = 0
-window_width = 1200
-window_height = 900
-
-# Initializing visualization - this is for non-notebook visualization
-glfw.init()
-window = glfw.create_window(window_width, window_height, "Simulation", None, None)
-glfw.make_context_current(window) # tells openGL (graphics) which window to use
-glfw.swap_interval(1) # setting the refresh cycle
-
-mj.mjv_defaultCamera(cam)
-mj.mjv_defaultOption(opt)
-scene = mj.MjvScene(model, maxgeom=10000) # This is setting up the scene based on our XML file
-context = mj.MjrContext(model,mj.mjtFontScale.mjFONTSCALE_150.value)
-
-glfw.set_key_callback(window, keyboard)
-glfw.set_cursor_pos_callback(window, mouse_move)
-glfw.set_mouse_button_callback(window, mouse_button)
-glfw.set_scroll_callback(window, scroll)
-
-# SETTING UP VIEWABLE ENVIRONMENT (DEACTIVATE IF WANTED)
-while not glfw.window_should_close(window):
-    time_prev = data.time
-
-    while (data.time - time_prev < 1.0/60.0):
-        mj.mj_step(model, data)
-
-    if (data.time>=simulation_time):
-        break;
-
-    # get framebuffer viewport
-    viewport_width, viewport_height = glfw.get_framebuffer_size(
-        window)
-    viewport = mj.MjrRect(0, 0, viewport_width, viewport_height)
-
-    #print camera configuration (help to initialize the view)
-    if (print_camera_config==1):
-        print('cam.azimuth =',cam.azimuth,';','cam.elevation =',cam.elevation,';','cam.distance = ',cam.distance)
-        print('cam.lookat =np.array([',cam.lookat[0],',',cam.lookat[1],',',cam.lookat[2],'])')
-
-    cam.lookat[0] = data.qpos[0]
-    # Update scene and render
-    mj.mjv_updateScene(model, data, opt, None, cam,
-                       mj.mjtCatBit.mjCAT_ALL.value, scene)
-    mj.mjr_render(viewport, scene, context)
-
-    # swap OpenGL buffers (blocking call due to v-sync)
-    glfw.swap_buffers(window)
-
-    # process pending GUI events, call GLFW callbacks
-    glfw.poll_events()
-
-glfw.terminate()
-
+render(xml_path, 7, 20, "track")
